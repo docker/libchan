063d6696e4e2915a7c5d24670a784b47fdab6d37
Alexandr Morozov <lk4d4math@gmail.com>
2014-06-11 15:03:13 +0400

    Fix TestSetMessageTwice failing because map is unordered
    Also added some more checks to data/message_test.go
    Docker-DCO-1.1-Signed-off-by: Alexandr Morozov <lk4d4math@gmail.com> (github: LK4D4)

--- data/message_test.go
+++ data/message_test.go
@@ -25,14 +25,22 @@ func TestSetMessage(t *testing.T) {
 	if len(decodedOutput) != 1 {
 		t.Fatalf("wrong output data: %#v\n", decodedOutput)
 	}
+	if len(decodedOutput["foo"]) != 1 {
+		t.Fatalf("Key foo contains %d values, expected 1", len(decodedOutput["foo"]))
+	}
+	if decodedOutput["foo"][0] != "bar" {
+		t.Fatalf("Key foo is %v, expected bar", decodedOutput["foo"][0])
+	}
 }
 
 func TestSetMessageTwice(t *testing.T) {
 	m := Empty().Set("foo", "bar").Set("ga", "bu")
 	output := m.String()
-	expectedOutput := "000;3:foo,6:3:bar,,2:ga,5:2:bu,,"
-	if output != expectedOutput {
-		t.Fatalf("'%v' != '%v'", output, expectedOutput)
+	// Check two outputs because map is unordered
+	expectedOutput1 := "000;3:foo,6:3:bar,,2:ga,5:2:bu,,"
+	expectedOutput2 := "000;2:ga,5:2:bu,,3:foo,6:3:bar,,"
+	if output != expectedOutput1 && output != expectedOutput2 {
+		t.Fatalf("'%v' != '%v' and '%v' != '%v'", output, expectedOutput1, output, expectedOutput2)
 	}
 	decodedOutput, err := Decode(output)
 	if err != nil {
@@ -40,6 +48,18 @@ func TestSetMessageTwice(t *testing.T) {
 	}
 	if len(decodedOutput) != 2 {
 		t.Fatalf("wrong output data: %#v\n", decodedOutput)
+	}
+	if len(decodedOutput["foo"]) != 1 {
+		t.Fatalf("Key foo contains %d values, expected 1", len(decodedOutput["foo"]))
+	}
+	if len(decodedOutput["ga"]) != 1 {
+		t.Fatalf("Key ga contains %d values, expected 1", len(decodedOutput["ga"]))
+	}
+	if decodedOutput["foo"][0] != "bar" {
+		t.Fatalf("Key foo is %v, expected bar", decodedOutput["foo"][0])
+	}
+	if decodedOutput["ga"][0] != "bu" {
+		t.Fatalf("Key ga is %v, expected bu", decodedOutput["ga"][0])
 	}
 }
 
