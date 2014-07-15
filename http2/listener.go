01cb5a06e0335733256a5dd3689bce2fa37c6866
Derek McGowan <derek@mcgstyle.net>
2014-07-15 15:26:06 -0700

    Clean up old http2 implementation
    Signed-off-by: Derek McGowan <derek@mcgstyle.net> (github: dmcgowan)

--- http2/listener.go
+++ http2/listener.go
@@ -2,55 +2,18 @@ package http2
 
 import "net"
 
-// ListenSession is a session manager which accepts new
-// connections and spawns spdy connection managers.
-type ListenSession struct {
-	listener net.Listener
-	auth     Authenticator
-}
-
-// NewListenSession creates a new listen session using
-// a network listeners and function to authenticate
-// new connections.  ListenSession expects tls session
-// handling to occur by the authenticator or the listener,
-// ListenSession will not perform tls handshakes.
-func NewListenSession(listener net.Listener, auth Authenticator) (*ListenSession, error) {
-	return &ListenSession{
-		listener: listener,
-		auth:     auth,
-	}, nil
-}
-
-// Close closes the underlying listener
-func (l *ListenSession) Close() error {
-	return l.listener.Close()
-}
-
-// AcceptSessions waits for a new network connections
-// and creates a new stream.  Connections which fail
-// authentication will not be returned.
-func (l *ListenSession) AcceptSession() (*StreamSession, error) {
-	for {
-		conn, err := l.listener.Accept()
-		if err != nil {
-			return nil, err
-		}
-		authErr := l.auth(conn)
-		if authErr != nil {
-			// TODO log
-			conn.Close()
-			continue
-		}
-
-		return newStreamSession(conn, true)
-	}
-}
-
+// SessionListener is a listener which accepts new
+// connections angi rd spawns spdy sessions.
 type SessionListener struct {
 	listener net.Listener
 	auth     Authenticator
 }
 
+// NewSessionListener creates a new listen session using
+// a network listeners and function to authenticate
+// new connections.  SessionListener expects tls session
+// handling to occur by the authenticator or the listener,
+// SessionListener will not perform tls handshakes.
 func NewSessionListener(listener net.Listener, auth Authenticator) (*SessionListener, error) {
 	return &SessionListener{
 		listener: listener,
--- http2/spdy.go
+++ http2/spdy.go
@@ -5,16 +5,7 @@
 package http2
 
 import (
-	"encoding/base64"
-	"errors"
-	"fmt"
-	"io"
 	"net"
-	"net/http"
-	"os"
-	"syscall"
-
-	"github.com/docker/spdystream"
 )
 
 // Authenticator is a function to provide authentication to
@@ -29,52 +20,4 @@ type Authenticator func(conn net.Conn) error
 // be secure.
 func NoAuthenticator(conn net.Conn) error {
 	return nil
-}
-
-type streamChanProvider interface {
-	addStreamChan(stream *spdystream.Stream, streamChan chan *spdystream.Stream)
-	getStreamChan(stream *spdystream.Stream) chan *spdystream.Stream
-}
-
-func extractDataHeader(headers http.Header) ([]byte, error) {
-	dataString := headers["Data"]
-	if len(dataString) != 1 {
-		if len(dataString) == 0 {
-			return nil, errors.New("missing data header")
-		} else {
-			return nil, errors.New("multiple data headers")
-		}
-	}
-	data, decodeErr := base64.URLEncoding.DecodeString(dataString[0])
-	if decodeErr != nil {
-		return nil, decodeErr
-	}
-	return data, nil
-}
-
-func createAttachment(stream *spdystream.Stream) (*os.File, error) {
-	if stream.IsFinished() {
-		return nil, fmt.Errorf("stream already finished")
-	}
-
-	socketFds, socketErr := syscall.Socketpair(syscall.AF_LOCAL, syscall.SOCK_STREAM|syscall.FD_CLOEXEC, 0)
-	if socketErr != nil {
-		return nil, socketErr
-	}
-	pipe := os.NewFile(uintptr(socketFds[1]), "")
-	defer pipe.Close()
-	conn, connErr := net.FileConn(pipe)
-	if connErr != nil {
-		return nil, connErr
-	}
-
-	go func() {
-		io.Copy(conn, stream)
-		conn.Close()
-	}()
-	go func() {
-		io.Copy(stream, conn)
-	}()
-
-	return os.NewFile(uintptr(socketFds[0]), ""), nil
 }
